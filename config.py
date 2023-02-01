fd3a199b2a7b857fe72cb01ea6d7f4eeeec8d643
Anshuman Prasad Mahanta <anshumanprasadmahanta@gmail.com>
2023-02-01 14:23:23 +0530

    No Need

--- config.py
+++ config.py
@@ -9,23 +9,6 @@
 #
 
 from os import environ
-from requests import get as rget
-
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
-try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('.env', 'wb+') as f:
-                f.write(res.content)
-        else:
-            log_error(f"Failed to download config.env {res.status_code}")
-    except Exception as e:
-        log_error(f"CONFIG_FILE_URL: {e}")
-except:
-    pass
 
 from dotenv import load_dotenv
 
